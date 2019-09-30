bb0ee868e83a238103de964732572b5a2d3803da
martino <martino.trevisan@studenti.polito.it>
2019-09-30 10:05:25 +0200

    first files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# pyspeedtest
-Simple package that uses Selenium to automate tests from speedtest.net
+PySpeedTest
+===========
+
+PySpeedTest is a simple package that uses Selenium to automate tests from speedtest.net.
+
+## Requirements
+
+You need Python3 with the `selenium` package. Be sure that WebDrivers are in your system `$PATH`.
+
+To use `tcpdump` and get traffic traces, you need to execute it as `root`, or, better, give the `suid` flag to the `tcpdump` executable.
+
+
+## Example
+You can run a test with:
+```
+results_chrome  = pyspeedtest.run_speedtest(browser="chrome", pcap_path="trace.pcap")
+print(results_chrome)
+```
---
+++ pyspeedtest/__init__.py
@@ -0,0 +1,64 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+import subprocess
+import time
+
+SHORT_TIME=2
+LONG_TIME=120
+XPATH_BANNER='//*[@id="_evidon-banner-acceptbutton"]'
+XPATH_GO='//*[@id="container"]/div[2]/div/div/div/div[3]/div[1]/div[1]/a/span[4]'
+XPATH_PING     = '/html/body/div[3]/div[2]/div/div/div/div[3]/div[1]/div[3]/div/div[3]/div/div[1]/div[2]/div[1]/div/div[2]/span'
+XPATH_DOWNLOAD = '/html/body/div[3]/div[2]/div/div/div/div[3]/div[1]/div[3]/div/div[3]/div/div[1]/div[2]/div[2]/div/div[2]/span'
+XPATH_UPLOAD   = '/html/body/div[3]/div[2]/div/div/div/div[3]/div[1]/div[3]/div/div[3]/div/div[1]/div[2]/div[3]/div/div[2]/span'
+XPATH_ORG='//*[@id="container"]/div[2]/div/div/div/div[3]/div[1]/div[3]/div/div[4]/div/div[2]/div/div[1]/div[2]'
+XPATH_IP='//*[@id="container"]/div[2]/div/div/div/div[3]/div[1]/div[3]/div/div[4]/div/div[2]/div/div[1]/div[3]'
+TCPDUMP_CMD="tcpdump -i any -w {} {}"
+KILL_TCPDUMP="killall -INT tcpdump"
+
+def run_speedtest(browser="chrome", pcap_path="/tmp/a.pcap", pcap_opt="-s 60"):
+
+    # Start Capture
+    tcpdump = subprocess.Popen(TCPDUMP_CMD.format(pcap_path, pcap_opt), shell=True)
+
+    # Start WebDriver and go on https://www.speedtest.net/
+    if browser.lower() == "firefox":
+        driver = webdriver.Firefox()
+    elif browser.lower() == "chrome":
+        driver = webdriver.Chrome()
+    else:
+        print("Unsupported browser")
+        return
+    driver.get("https://www.speedtest.net/")
+
+    # Click Banner
+    elem = driver.find_elements_by_xpath(XPATH_BANNER)
+    elem[0].click()
+    time.sleep(SHORT_TIME)
+
+    # Click Go
+    elem = driver.find_elements_by_xpath(XPATH_GO)
+    elem[0].click()
+    time.sleep(SHORT_TIME)
+
+    # Wait Results
+    def num_there(s):
+        return any(i.isdigit() for i in s)
+    element = WebDriverWait(driver, LONG_TIME).until(lambda driver : num_there(driver.find_elements_by_xpath(XPATH_UPLOAD)[0].text))
+    time.sleep(SHORT_TIME)
+
+    # Get Results
+    ping     = driver.find_elements_by_xpath(XPATH_PING)[0].text
+    download = driver.find_elements_by_xpath(XPATH_DOWNLOAD)[0].text
+    upload   = driver.find_elements_by_xpath(XPATH_UPLOAD)[0].text
+    org      = driver.find_elements_by_xpath(XPATH_ORG)[0].text
+    sip      = driver.find_elements_by_xpath(XPATH_IP)[0].text
+
+    #print("Ping", ping, "ms")
+    #print("Download", download, "Mbps")
+    #print("Upload", upload, "Mbps")
+
+    # Close
+    driver.close()
+    _ = subprocess.call(KILL_TCPDUMP, shell=True)
+    
+    return(ping, download, upload, org, sip)
